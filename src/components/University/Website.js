7d0dff88eaecf49fc18febcc428c37df23f3722a
chrissyalbert <chrissy_albert@yahoo.com>
2020-03-02 17:10:42 -0700

    Update Website.js
    add http:// to url without it so that they open properly in new window

--- src/components/University/Website.js
+++ src/components/University/Website.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import Card from 'react-bootstrap/Card';
 
 export function Website(props) {
+  const [url, setUrl] = useState(props.university.URL);
+
+  useEffect(()=> {
+    if (url.indexOf("http://") === 0 || url.indexOf("https://") === 0) {
+      setUrl(props.university.URL)
+    }
+     else if (url.indexOf("http://") !== 0 || url.indexOf("https://") !== 0) {
+      let addHttp = `http://${url}`;
+      setUrl(addHttp);
+    }
+  
+  }, [props.university.URL, url]);
   return (
     <section className="University-website">
                       <h6>Website: </h6>
                       <Card.Text>
-                        <a href={props.university.URL} target="_blank" rel="noopener noreferrer" className="urlStyle">{props.university.URL}</a>
+                        <a href={url} target="_blank" rel="noopener noreferrer" className="urlStyle">{props.university.URL}</a>
                       </Card.Text>    
                   </section>
   );
